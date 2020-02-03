1956f4e154c8e99a6afa4c3892088ebe44473417
Nikolay Lukyanchikov <kalyastiy@gmail.com>
2020-02-03 09:35:44 +0300

    Initial Commit

---
+++ TestFlickr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestFlickr
+//
+//  Created by Nikolay Lukyanchikov on 03.02.2020.
+//  Copyright © 2020 Nikolay Lukyanchikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
